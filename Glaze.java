82235c87fef4bc90c54bf56a5a94d7abb592ec66
Arianne Wallace <ariannewallace@gmail.com>
2021-03-10 20:34:12 -0500

    pushing to github

---
+++ Glaze.java
@@ -0,0 +1,8 @@
+package donut;
+
+public class Glaze {
+    public static void main(String[] args) {
+        System.out.println("pushed to github successfully");
+    }
+}
+//C:\Users\Student\IdeaProjects\homework2\src\main\java\donut\Glaze.java
